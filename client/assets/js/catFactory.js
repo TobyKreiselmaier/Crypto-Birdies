01b57f9b04329b50cf963c5fa52b3030d4267c21
TobyKreiselmaier <63912793+TobyKreiselmaier@users.noreply.github.com>
2020-09-23 13:04:19 -0400

    Added functionality.

--- client/assets/js/catFactory.js
+++ client/assets/js/catFactory.js
@@ -1,5 +1,4 @@
-
-//Random color
+//Randomize color
 function getColor() {
     var randomColor = Math.floor(Math.random() * 16777215).toString(16);
     return randomColor;
@@ -38,38 +37,136 @@ function earsColor(color,code) {
     $('#dnaears').html(code) //This updates the body color part of the DNA that is displayed below the cat
 }
 
-//###################################################
-//Functions below will be used later on in the project
-//###################################################
 function eyeVariation(num) {
 
     $('#dnashape').html(num)
     switch (num) {
-        case 1:
-            normalEyes()
-            $('#eyeName').html('Basic')
-            break
+        case 0:
+            normalEyes();
+            $('#eyeDesign').html('Basic');
+            break;
+        case 1:    
+            normalEyes();
+            $('#eyeDesign').html('Chilled');
+            eyesType1();
+            break;
+        case 2:    
+            normalEyes();
+            $('#eyeDesign').html('Up');
+            eyesType2();
+            break;
+        case 3:    
+            normalEyes();
+            $('#eyeDesign').html('Right');
+            eyesType3();
+            break;    
+        case 4:    
+            normalEyes();
+            $('#eyeDesign').html('Left');
+            eyesType4();
+            break;    
+        case 5:    
+            normalEyes();
+            $('#eyeDesign').html('Bottom Left');
+            eyesType5();
+            break;    
+        case 6:    
+            normalEyes();
+            $('#eyeDesign').html('Top Right');
+            eyesType6();
+            break;    
+        case 7:    
+            normalEyes();
+            $('#eyeDesign').html('Bottom Right');
+            eyesType7();
+            break;    
+        case 8:    
+            normalEyes();
+            $('#eyeDesign').html('Top Left');
+            eyesType8();
+            break;    
+        case 9:    
+            normalEyes();
+            $('#eyeDesign').html('Special');
+            eyesType9();
+            break;    
+
     }
+}
+
+function normalEyes() {
+    $('.cat__eye').find('span').css('border', 'none');
+    $('.cat__eye').find('span').css('transform', 'none');
+    $('.cat__eye').find('span').css('background-image', 'none');
+}
+
+function eyesType1() {
+    $('.cat__eye').find('span').css('border-top', '15px solid');
+}
+
+function eyesType2() {
+    $('.cat__eye').find('span').css('border-bottom', '15px solid');
+}
+
+function eyesType3() {
+    $('.cat__eye').find('span').css('border-left', '15px solid');
+}
+
+function eyesType4() {
+    $('.cat__eye').find('span').css('border-right', '15px solid');
+}
+
+function eyesType5() {
+    $('.cat__eye').find('span').css('border-top', '15px solid');
+    $('.cat__eye').find('span').css('transform', 'rotate(30deg)');
+}
+
+function eyesType6() {
+    $('.cat__eye').find('span').css('border-bottom', '15px solid');
+    $('.cat__eye').find('span').css('transform', 'rotate(30deg)');
+}
+
+function eyesType7() {
+    $('.cat__eye').find('span').css('border-left', '15px solid');
+    $('.cat__eye').find('span').css('transform', 'rotate(30deg)');
+}
+
+function eyesType8() {
+    $('.cat__eye').find('span').css('border-right', '15px solid');
+    $('.cat__eye').find('span').css('transform', 'rotate(30deg)');
+}
+
+function eyesType9() {
+    $('.cat__eye').find('span').css('background-image', 'linear-gradient(to right, rgb(114, 108, 29) , rgb(31, 31, 61), rgb(114, 108, 29))');
+
+}
+
+function normaldecoration() {
+    $('.cat__head-dots').css({ "transform": "rotate(0deg)", "height": "48px", "width": "14px", "top": "1px", "border-radius": "0 0 50% 50%" })
+    $('.cat__head-dots_first').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "50% 0 50% 50%" })
+    $('.cat__head-dots_second').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "0 50% 50% 50%" })
 }
 
 function decorationVariation(num) {
     $('#dnadecoration').html(num)
     switch (num) {
         case 1:
-            $('#decorationName').html('Basic')
-            normaldecoration()
-            break
+            $('#decorationName').html('Basic');
+            normaldecoration();
+            break;
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
     }
-}
-
-async function normalEyes() {
-    await $('.cat__eye').find('span').css('border', 'none')
-}
-
-async function normaldecoration() {
-    //Remove all style from other decorations
-    //In this way we can also use normalDecoration() to reset the decoration style
-    $('.cat__head-dots').css({ "transform": "rotate(0deg)", "height": "48px", "width": "14px", "top": "1px", "border-radius": "0 0 50% 50%" })
-    $('.cat__head-dots_first').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "50% 0 50% 50%" })
-    $('.cat__head-dots_second').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "0 50% 50% 50%" })
 }
--- client/assets/js/catSettings.js
+++ client/assets/js/catSettings.js
@@ -15,37 +15,34 @@ var defaultDNA = {
     "lastNum" :  1
     }
 
-// when page loads
+// populate DNA when page loads
 $( document ).ready(function() {
   $('#dnabody').html(defaultDNA.headColor);
   $('#dnamouth').html(defaultDNA.mouthColor);
   $('#dnaeyes').html(defaultDNA.eyesColor);
   $('#dnaears').html(defaultDNA.earsColor);
-    
-//   $('#dnashape').html(defaultDNA.eyesShape)
-//   $('#dnadecoration').html(defaultDNA.decorationPattern)
-//   $('#dnadecorationMid').html(defaultDNA.decorationMidcolor)
-//   $('#dnadecorationSides').html(defaultDNA.decorationSidescolor)
-//   $('#dnaanimation').html(defaultDNA.animation)
-//   $('#dnaspecial').html(defaultDNA.lastNum)
-
-  renderCat(defaultDNA)
+  $('#dnashape').html(defaultDNA.eyesShape);
+  $('#dnadecoration').html(defaultDNA.decorationPattern);
+  $('#dnadecorationMid').html(defaultDNA.decorationMidcolor);
+  $('#dnadecorationSides').html(defaultDNA.decorationSidescolor);
+  $('#dnaanimation').html(defaultDNA.animation);
+  $('#dnaspecial').html(defaultDNA.lastNum);
+  renderCat(defaultDNA);
 });
 
 function getDna(){
-    var dna = ''
-    dna += $('#dnabody').html()
-    dna += $('#dnamouth').html()
-    dna += $('#dnaeyes').html()
-    dna += $('#dnaears').html()
-    dna += $('#dnashape').html()
-    dna += $('#dnadecoration').html()
-    dna += $('#dnadecorationMid').html()
-    dna += $('#dnadecorationSides').html()
-    dna += $('#dnaanimation').html()
-    dna += $('#dnaspecial').html()
-
-    return parseInt(dna)
+    var dna = '';
+    dna += $('#dnabody').html();
+    dna += $('#dnamouth').html();
+    dna += $('#dnaeyes').html();
+    dna += $('#dnaears').html();
+    dna += $('#dnashape').html();
+    dna += $('#dnadecoration').html();
+    dna += $('#dnadecorationMid').html();
+    dna += $('#dnadecorationSides').html();
+    dna += $('#dnaanimation').html();
+    dna += $('#dnaspecial').html();
+    return parseInt(dna);
 }
 
 function renderCat(dna){
@@ -54,12 +51,12 @@ function renderCat(dna){
     mouthColor(colors[dna.mouthColor],dna.mouthColor)
     $('#mouthcolor').val(dna.mouthColor)
     eyesColor(colors[dna.eyesColor],dna.eyesColor)
-    $('#mouthcolor').val(dna.eyesColor)
+    $('#eyescolor').val(dna.eyesColor)
     earsColor(colors[dna.earsColor],dna.earsColor)
-    $('#mouthcolor').val(dna.earsColor)
+    $('#earscolor').val(dna.earsColor)
 }
 
-// Changing cat colors
+// Event listeners
 $('#bodycolor').change(()=>{
     var colorVal = $('#bodycolor').val()
     headColor(colors[colorVal],colorVal)
@@ -78,4 +75,9 @@ $('#eyescolor').change(()=>{
 $('#earscolor').change(()=>{
   var colorVal = $('#earscolor').val()
   earsColor(colors[colorVal],colorVal)
+});
+
+$('#eyeInput').change(()=>{
+  var shape = parseInt($('#eyeInput').val())
+  eyeVariation(shape)
 });
