82954021b38b36dfc14322de8affbb623d30b838
TobyKreiselmaier <63912793+TobyKreiselmaier@users.noreply.github.com>
2020-09-23 05:31:05 -0400

    Add files via upload

---
+++ client/assets/js/catSettings.js
@@ -0,0 +1,60 @@
+
+var colors = Object.values(allColors())
+
+var defaultDNA = {
+    "headcolor" : 10,
+    "mouthColor" : 13,
+    "eyesColor" : 96,
+    "earsColor" : 10,
+    //Cattributes
+    "eyesShape" : 1,
+    "decorationPattern" : 1,
+    "decorationMidcolor" : 13,
+    "decorationSidescolor" : 13,
+    "animation" :  1,
+    "lastNum" :  1
+    }
+
+// when page load
+$( document ).ready(function() {
+  $('#dnabody').html(defaultDNA.headColor);
+  $('#dnamouth').html(defaultDNA.mouthColor);
+  $('#dnaeyes').html(defaultDNA.eyesColor);
+  $('#dnaears').html(defaultDNA.earsColor);
+    
+//   $('#dnashape').html(defaultDNA.eyesShape)
+//   $('#dnadecoration').html(defaultDNA.decorationPattern)
+//   $('#dnadecorationMid').html(defaultDNA.decorationMidcolor)
+//   $('#dnadecorationSides').html(defaultDNA.decorationSidescolor)
+//   $('#dnaanimation').html(defaultDNA.animation)
+//   $('#dnaspecial').html(defaultDNA.lastNum)
+
+  renderCat(defaultDNA)
+});
+
+function getDna(){
+    var dna = ''
+    dna += $('#dnabody').html()
+    dna += $('#dnamouth').html()
+    dna += $('#dnaeyes').html()
+    dna += $('#dnaears').html()
+    dna += $('#dnashape').html()
+    dna += $('#dnadecoration').html()
+    dna += $('#dnadecorationMid').html()
+    dna += $('#dnadecorationSides').html()
+    dna += $('#dnaanimation').html()
+    dna += $('#dnaspecial').html()
+
+    return parseInt(dna)
+}
+
+function renderCat(dna){
+    headColor(colors[dna.headcolor],dna.headcolor)
+    $('#bodycolor').val(dna.headcolor)
+}
+
+// Changing cat colors
+$('#bodycolor').change(()=>{
+    var colorVal = $('#bodycolor').val()
+    headColor(colors[colorVal],colorVal)
+})
