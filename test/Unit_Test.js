1a36db4cc58b6327e96514bce03f63385670d24e
TobyKreiselmaier <63912793+TobyKreiselmaier@users.noreply.github.com>
2020-09-29 14:17:19 -0400

    Improved Unit Testing

--- test/Unit_Test.js
+++ test/Unit_Test.js
@@ -3,40 +3,49 @@ const Birdcontract = artifacts.require("Birdcontract");
 contract("Birdcontract", async accounts => {
   it("should have a native token called 'AngryBirdontheBlock'", async () => {
     var instance = await Birdcontract.deployed();
-    var testName = await instance._name;
+    var testName = await instance.name();
     assert.equal(testName, "AngryBirdontheBlock", "Token wasn't constructed correctly");
   });
 
 
   it("should have the token symbol 'ABBX'", async () => {
     var instance = await Birdcontract.deployed();
-    var testSymbol = await instance._symbol;
+    var testSymbol = await instance.symbol();
     assert.equal(testSymbol, "ABBX", "Symbol wasn't constructed correctly");
   });
 
   it("should record the number of tokens owned by an address correctly", async () => {
     var instance = await Birdcontract.deployed();
-    var numberTokens = await instance.ownsNumberOfTokens[accounts[0]];
+    var numberTokens = await instance.balanceOf(accounts[0]);
     assert.equal(numberTokens, 0, "The record of tokens is flawed and can't be trusted");
-    numberTokens = await instance.ownsNumberOfTokens[accounts[0]]++;
+    await instance.createBirdGen0(123456);
+    numberTokens = await instance.balanceOf(accounts[0]);
     assert.equal(numberTokens, 1, "The record of tokens is flawed and can't be trusted");
-    numberTokens = await instance.ownsNumberOfTokens[accounts[0]]--;
-    assert.equal(numberTokens, 0, "The record of tokens is flawed and can't be trusted");
+
   });
 
   it("should record the total supply of tokens correctly", async () => {
     var instance = await Birdcontract.deployed();
-    var supply = await instance.birdies.length();
-    assert.equal(supply, 0, "The record of total supply is flawed and can't be trusted");
-    //add code to create a dummie token and test the total supply again.
+    var supply = await instance.totalSupply();
+    assert.equal(supply, 1, "The record of total supply is flawed and can't be trusted");
+    //a bird was created in the previous test.
   });
 
   it("should transfer a token correctly", async () => {
     var instance = await Birdcontract.deployed();
-    //add code to create a dummie token and test:
-    //- contract only allows owner to transfer
-    //- transfer to the burn address is not possible
-    //- transfer to the contract address is not possible
-    //- records for both sender and recipient are correctly updated
+    var testOwner = await instance.ownerOf(0);
+    assert.equal(testOwner, accounts[0], "Owner is not msg.sender");
+    var testTransfer = await instance.transfer(address(0), 0);
+    assert.equal(testTransfer, false, "Error: Token was transferred to the burn address");
+    testTransfer = await instance.transfer(address(this), 0);
+    assert.equal(testTransfer, false, "Error: Token was transferred to the contract address");
+    var numberTokensSenderStart = await instance.balanceOf(accounts[0]);
+    var numberTokensRecipientStart = await instance.balanceOf(accounts[1]);
+    var testTransfer = await instance.transfer(accounts[1], 0);
+    assert.equal(testTransfer, true, "Token wasn't transferred correctly");
+    var numberTokensSenderEnd = await instance.balanceOf(accounts[0]);
+    assert.equal(numberTokensSenderEnd, numberTokensSenderStart--, "Token balance sender is not updated correctly");
+    var numberTokensRecipientEnd = await instance.balanceOf(accounts[1]);
+    assert.equal(numberTokensRecipientEnd, numberTokensRecipientStart++, "Token balance sender is not updated correctly");
   });
 })
