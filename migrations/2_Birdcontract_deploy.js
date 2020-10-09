537a984147d8af9de49c8062bc20d777f547e750
TobyKreiselmaier <63912793+TobyKreiselmaier@users.noreply.github.com>
2020-10-09 15:12:46 -0400

    Delete 2_Birdcontract_deploy.js

--- migrations/2_Birdcontract_deploy.js
+++
@@ -1,9 +0,0 @@
-const ERC721 = artifacts.require("Birdcontract");
-
-module.exports = async function(deployer, network, accounts) {
-  await deployer.deploy(ERC721, "AngryBirdontheBlock", "ABBX");
-  const instance = await ERC721.deployed();
-  if(instance) {
-    console.log("Success.")
-  }
-};
