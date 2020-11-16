1a4307b205b1c366936748af3894e1cbe3f2bda9
TobyKreiselmaier <63912793+TobyKreiselmaier@users.noreply.github.com>
2020-11-16 16:38:43 -0500

    Removed error

--- client/assets/js/blockchain.js
+++ client/assets/js/blockchain.js
@@ -11,10 +11,8 @@ var marketAddress = "0x78ad2f9c3924278692125a23ed05d4e5facfd97c"; // Ropsten: 0x
 async function connectWallet() {
     return window.ethereum.enable().then(function(accounts){
         user = accounts[0];
-        birdInstance = new web3.eth.Contract(abi.birdContract, birdAddress, user, 
-            {from: user});
-        marketInstance = new web3.eth.Contract(abi.marketContract, marketAddress, 
-            user, {from: user});
+        birdInstance = new web3.eth.Contract(abi.birdContract, birdAddress, {from: user});
+        marketInstance = new web3.eth.Contract(abi.marketContract, marketAddress, {from: user});
 
         birdInstance.events.Birth()
             .on('data', async function (event) {
